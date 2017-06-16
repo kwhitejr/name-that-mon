904504237bc731807164736ebb1a3c92c8fbf83f
kwhitejr <kwhitejr.edu@gmail.com>
2017-06-16 15:56:39 -0700

    achieved basic layout for answer selection

--- src/Components/answerSelection.js
+++ src/Components/answerSelection.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
+// For simple styles, no need for separate CSS file
 const style = {
   margin: 12,
 };
@@ -10,12 +12,29 @@ class AnswerSelection extends Component {
   render() {
 
     return (
-      <div>
-        <RaisedButton label="Squirtle" primary={true} style={style} />
-        <RaisedButton label="Bulbasaur" primary={true} style={style} />
-        <RaisedButton label="Charizard" primary={true} style={style} />
-        <RaisedButton label="Pikachu" primary={true} style={style} />
-      </div>
+      <Grid fluid>
+        <Row>
+          <Col xs={6} >
+            <RaisedButton label="Squirtle" primary={true} style={style} fullWidth={true} />
+          </Col>
+          <Col xs={6} >
+            <RaisedButton label="Bulbasaur" primary={true} style={style} fullWidth={true} />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} >
+            <RaisedButton label="Charizard" primary={true} style={style} fullWidth={true} />
+          </Col>
+          <Col xs={6} >
+            <RaisedButton label="Pikachu" primary={true} style={style} fullWidth={true} />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} >
+            <RaisedButton label="Submit" secondary={true} style={style} fullWidth={true} />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
-import AnswerSelection from '../Components/answerSelection'
+import AnswerSelection from '../Components/answerSelection';
 
 import pokemon from '../assets/pokemon/1.png';
 import './Quiz.css';
 
+const style = {
+  margin: 12,
+}
+
 class Quiz extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={pokemon} className="mask" alt="logo" />
-          <h2>Who is this?</h2>
+      <Grid>
+        <div className="App">
+          <Row>
+            <div className="App-header">
+              <img src={pokemon} className="mask" alt="logo" />
+              <h2>Who is this?</h2>
+            </div>
+          </Row>
+          <Row>            
+            <AnswerSelection />
+          </Row>
         </div>
-        <AnswerSelection />
-        <button>Submit</button>
-        <Link to="/">Cancel</Link>
-      </div>
+      </Grid>
     );
   }
 }
