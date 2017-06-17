08b781df0a23e2796ff2e7102161d4106238497c
kwhitejr <kwhitejr.edu@gmail.com>
2017-06-16 18:37:20 -0700

    added comments to store.js

--- src/store.js
+++ src/store.js
@@ -3,18 +3,27 @@ import thunk from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
 import { BrowserHistory } from 'react-history';
 
+/* "Reducer" is redux-speak for the set of functions that alter the application state. 
+*  State-altering functions should be grouped by purpose, thus the application may have multiple reducers.
+*  Our app is relatively straightforward, so we will likely only need one or two.
+*  For example, we should have a Quiz reducer.
+*/
 import rootReducer from './reducers';
 
+// There are multiple ways to do history in React, i.e. how to handle when a user selects Back in the browser rather than an in-app button.
 const router = routerMiddleware(BrowserHistory);
 
+// `thunk` is middleware that permits asynchronous actions within redux.
 const middleware = [thunk, router];
 
+// Devtools
 const enhancers = compose(
   applyMiddleware(...middleware),
   (window.devToolsExtension && process.env.NODE_ENV !== 'production') ?
     window.devToolsExtension() : f => f
 );
 
+// The `store` is where Redux manages state 
 export default function configureStore(initialState = {}) {
   const store = createStore(rootReducer, initialState, enhancers); 
 
