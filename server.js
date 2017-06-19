6074206a6d4d278c26b73771015422e50437868a
YuchinP <yuchinspan@gmail.com>
2017-06-19 15:19:47 -0700

    adjusted calling formula/server folders

---
+++ server.js
@@ -0,0 +1,34 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Sh1rogane',
+  database: 'pokemon'
+})
+
+var pokeArray = [];
+for (var i = 1; i <= 151; i++) {
+  pokeArray.push(i)
+}
+
+// function getRandomArbitrary(min, max) {
+//     return Math.random() * (151 - 1) + 1;
+// }
+//
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (151 - 1 + 1)) + 1;
+// }
+
+var specCalled = pokeArray[Math.floor(Math.random() * pokeArray.length)];
+
+console.log(specCalled);
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+  var sql = "SELECT species FROM pokemon where id = ?";
+  connection.query(sql,specCalled, function(err, results) {
+    if (err) throw err
+    console.log(results[0].species)
+  })
+})
