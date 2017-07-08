7da1c4714f828c4aa3e7b5f11e39171ed64a80b3
kwhitejr <kwhitejr.edu@gmail.com>
2017-07-08 16:02:46 -0700

    added basic pokemon retrieval gets

--- server/app.js
+++ server/app.js
@@ -8,6 +8,12 @@ const Pokemon = db.Pokemon;
 
 const app = express();
 
+// TODO: move these functions to `common.js`
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+/************/
+
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
@@ -15,16 +21,44 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/api/pokemon/:id', function (req, res) {
-  Pokemon.find({where: {id: req.params.id}})
-    .then(function (result) {
+// Get Pokemon by ID number
+app.get('/api/pokemon/id/:id', (req, res) => {
+  Pokemon.find({where: { id: req.params.id }})
+    .then( (result) => {
       res.send(result);
     });
 });
 
-app.get('/api/generation/:id', function (req, res) {
-  Pokemon.findAll({where: {generation: req.params.id}})
-    .then(function (result) {
+// Get Pokemon by name
+app.get('/api/pokemon/name/:name', (req, res) => {
+  const name = capitalizeFirstLetter(req.params.name);
+  Pokemon.find({where: { name: name }})
+    .then( (result) => {
+      res.send(result);
+    });
+});
+
+// Get Pokemon by generation
+app.get('/api/pokemon/generation/:generation', (req, res) => {
+  Pokemon.findAll({where: { generation: req.params.generation }})
+    .then( (result) => {
+      res.send(result);
+    });
+});
+
+// Get Pokemon by type
+app.get('/api/pokemon/type/:type', (req, res) => {
+  const type = capitalizeFirstLetter(req.params.type);
+  Pokemon.findAll({where: { type1: type }})
+    .then( (result) => {
+      res.send(result);
+    });
+});
+
+// Get Legendary Pokemon
+app.get('/api/pokemon/legendary', (req, res) => {
+  Pokemon.findAll({where: { legendary: true }})
+    .then( (result) => {
       res.send(result);
     });
 });
