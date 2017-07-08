462ea2f382012f5f8dba6bb1865c747d42cd1ab0
kwhitejr <kwhitejr.edu@gmail.com>
2017-07-07 20:09:38 -0700

    created pokemon model

---
+++ server/models/pokemon.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+  var Pokemon = sequelize.define('Pokemon', {
+    id:               DataTypes.INTEGER,
+    name:             DataTypes.STRING,
+    type1:            DataTypes.STRING,
+    type2:            DataTypes.STRING,
+    total:            DataTypes.INTEGER,
+    hp:               DataTypes.INTEGER,
+    attack:           DataTypes.INTEGER,
+    defense:          DataTypes.INTEGER,
+    special_attack:   DataTypes.INTEGER,
+    special_defense:  DataTypes.INTEGER,
+    speed:            DataTypes.INTEGER,
+    generation:       DataTypes.INTEGER,
+    legendary:        DataTypes.BOOLEAN
+  });
+
+  return Pokemon;
+};
