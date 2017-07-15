3d373f4b66d38402ec8ef5a5037da84d729411ec
kwhitejr <kwhitejr.edu@gmail.com>
2017-07-14 17:51:57 -0700

    result view ready for selector

--- src/Result/ResultTable.js
+++ src/Result/ResultTable.js
@@ -13,16 +13,20 @@ const ResultsTable = ({ correctAnswerStack, lastCorrectAnswer, endedOn }) => (
   <Table selectable={false} >
     <TableBody displayRowCheckbox={false} >
       <TableRow>
-        <TableRowColumn>Answer Streak</TableRowColumn>
-        <TableRowColumn>{correctAnswerStack.length}</TableRowColumn>
-      </TableRow>
-      <TableRow>
         <TableRowColumn>Ended On</TableRowColumn>
         <TableRowColumn>{endedOn.name}</TableRowColumn>
       </TableRow>
       <TableRow>
-        <TableRowColumn>Last Correct Answer</TableRowColumn>
-        <TableRowColumn>{lastCorrectAnswer.name}</TableRowColumn>
+        <TableRowColumn>Answer Streak</TableRowColumn>
+        <TableRowColumn>{correctAnswerStack.length}</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>Total Time</TableRowColumn>
+        <TableRowColumn>{null}</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>Avg Time Per Answer</TableRowColumn>
+        <TableRowColumn>{null}</TableRowColumn>
       </TableRow>
     </TableBody>
   </Table>
