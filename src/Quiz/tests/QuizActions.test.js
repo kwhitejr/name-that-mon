8a94f2767dd9ee24c8f341b1d1b0e16323e9cc7d
Kevin White <kwhitejr.edu@gmail.com>
2017-07-30 08:56:34 -0700

    began adding action tests, nock failing on proxy

---
+++ src/Quiz/tests/QuizActions.test.js
@@ -0,0 +1,127 @@
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+import nock from 'nock'
+
+import * as types from '../QuizActionTypes'
+import {
+	reset,
+	beginGenerationQuiz,
+	beginPokemonTypeQuiz,
+	beginLegendaryQuiz,
+	// getAnswerChoices,
+	setAnswer,
+	setNextQuestion,
+	stackCorrectAnswer,
+	submitAnswer,
+	useClue,
+	endCurrentQuiz,
+	postPlaythruData,
+} from '../QuizActions'
+
+const API_URL = 'http://localhost:3000/api/';
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+describe('actions', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  // reset
+  it('reset dispatches RESET_INSTANCE_DATA & RESET_META_DATA', () => {
+
+    const expectedActions = [
+      { type: types.RESET_INSTANCE_DATA },
+      { type: types.RESET_META_DATA },
+    ]
+    const store = mockStore({ 
+    	quizMetaData: {
+    	},
+    	quizInstance: {
+    	}
+    })
+
+    store.dispatch(reset())
+	  expect(store.getActions()).toEqual(expectedActions)
+  })
+
+  // beginGenerationQuiz
+  it('beginGenerationQuiz gets Generation pokemon set', () => {
+  	const generationNumber = 1
+
+  	nock(API_URL)
+      .get(`/pokemon/generation/`)
+      .query({ generation: generationNumber})
+      .reply(200, { body: { shuffledQuizStack: [{},{},{},{},{}] } })
+
+    const expectedActions = [
+    	{ type: types.GET_QUIZ_DATA, payload: [{},{},{},{},{}] },
+      { type: types.SET_GENERATION_NUMBER, payload: 1 },
+      { type: types.SET_ANSWER_CHOICES, payload: [{},{},{},{}] },
+      { type: types.START_TIMER, payload: 123456789098 },
+    ]
+
+    const store = mockStore({
+    	quizInstance: {
+    		shuffledQuizStack: [],
+    		answerChoices: [],
+    		startTime: null,
+			},
+			quizMetaData: {
+				quizType: null,
+				quizSet: null,
+			}
+    })
+
+    return store.dispatch(beginGenerationQuiz(generationNumber)).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  // TODO: beginPokemonTypeQuiz
+
+  // TODO: beginLegendaryQuiz
+
+  // setAnswer
+  it('setAnswer dispatches SET_ANSWER', () => {
+  	const event = {},
+  				value = 1;
+
+    const expectedActions = [
+      { type: types.SET_ANSWER, payload: 1 },
+    ]
+
+    const store = mockStore({ 
+    	quizMetaData: {
+    	},
+    	quizInstance: {
+    	}
+    })
+
+    store.dispatch(setAnswer(event, value))
+	  expect(store.getActions()).toEqual(expectedActions)
+  })
+
+  // setNextQuestion
+  it('setNextQuestion dispatches INCREMENT_CLUE_COUNT, STACK_CORRECT_ANSWER, & SET_ANSWER_CHOICES', () => {
+    const expectedActions = [
+      { type: types.INCREMENT_CLUE_COUNT },
+      { type: types.STACK_CORRECT_ANSWER, payload: {shuffledQuizStack: [{},{},{},{}], correctAnswerStack: [{},{}]} },
+      { type: types.SET_ANSWER_CHOICES, payload: [{},{},{},{}] },
+    ]
+
+    const store = mockStore({ 
+    	quizMetaData: {
+    	},
+    	quizInstance: {
+    		shuffledQuizStack: [{},{},{},{},{}],
+    		correctAnswerStack: [{}]
+    	}
+    })
+
+    store.dispatch(setNextQuestion())
+	  expect(store.getActions()).toEqual(expectedActions)
+  })
+})
