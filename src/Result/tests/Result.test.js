05435f6822ab4cb0e862a32b2c6118931af12d57
Kevin White <kwhitejr.edu@gmail.com>
2017-07-30 08:57:13 -0700

    moved Result test

--- src/Result/tests/Result.test.js
+++ src/Result/tests/Result.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Result from './Result';
+import Result from '../Result';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
