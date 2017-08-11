beef3c8fc630deecd7353c28703f0ad532a9cf70
Kevin White <kwhitejr.edu@gmail.com>
2017-08-11 16:09:04 -0700

    updated postgres script

--- server/create_pokemon_database.sql
+++ server/create_pokemon_database.sql
@@ -20,9 +20,32 @@ CREATE TABLE pokemon(
    legendary boolean
 );
 
-copy pokemon from '/Users/kevin/git/name-that-mon/server/pokemon-data.csv' DELIMITERS ',' CSV HEADER;
+-- copy pokemon from '/Users/kevin/git/name-that-mon/server/pokemon-data.csv' DELIMITERS ',' CSV HEADER;
 -- copy pokemon from '/Users/kwhi32/Projects/name-that-mon/server/pokemon-data.csv' DELIMITERS ',' CSV HEADER;
+copy pokemon from './server/pokemon-data.csv' DELIMITERS ',' CSV HEADER;
 
+DROP TABLE IF EXISTS playthru;
+
+CREATE TABLE playthru(
+   id serial primary key,
+   user_initials text,
+   quiz_type text not null,
+   quiz_set text,
+   start_time bigint,
+   end_time bigint,
+   clue_count int,
+   correct_answer_stack int[], -- array of pokemon ids
+   wrong_answer int REFERENCES pokemon (id) -- pokemon id
+);
+
+DROP TABLE IF EXISTS answer;
+
+CREATE TABLE answer(
+  id serial primary key,
+  pokemon_id int REFERENCES pokemon (id),
+  playthru_id int REFERENCES playthru (id),
+  was_correct boolean
+);
 
 -- pg_ctl -D /usr/local/var/postgres9.6 -l logfile start
 
