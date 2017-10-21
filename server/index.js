afc33633f56b5b42bfd2a713def568fdac4788c3
Kevin White <kwhitejr.edu@gmail.com>
2017-10-21 13:14:40 -0700

    updated homepage in package json

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const db = require('./models');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001;
 
 // app.listen(PORT, () => {
 //   console.log(`App listening on port ${PORT}!`);
